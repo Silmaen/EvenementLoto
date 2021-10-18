f4d593e2d373c2a83b60f71d382bd0556f2e2d1c
Silmaen <genteur.slayer@laposte.net>
2021-10-19 00:14:27 +0200

    better gitignore

---
+++ include/UI/About.h
@@ -0,0 +1,38 @@
+/**
+ * @author argawaen 
+ * @date 18/10/2021
+ */
+#pragma once
+
+#include <QDialog>
+
+namespace Ui {
+/**
+ * @brief La classe encapsulant le contenu du fichier UI
+ */
+class About;
+}// namespace Ui
+
+namespace evl {
+
+/**
+ * @brief Classe D'affichage de la boite de dialogue d'à propos.
+ */
+class About: public QDialog {
+    Q_OBJECT
+public:
+    /**
+     * @brief Constructeur.
+     * @param parent Le widget Parent.
+     */
+    explicit About(QWidget* parent= nullptr);
+    /**
+     * @brief Destructeur.
+     */
+    ~About() override;
+
+private:
+    Ui::About* ui;///< Lien vers la page UI.
+};
+
+}// namespace evl
---
+++ sources/UI/About.cpp
@@ -0,0 +1,22 @@
+/**
+ * @author Silmaen
+ * @created 18/10/2021
+ */
+#include "UI/About.h"
+
+// Les trucs de QT
+#include "UI/moc_About.cpp"
+#include "UI/ui_About.h"
+
+namespace evl {
+
+About::About(QWidget* parent):
+    QDialog(parent),
+    ui(new Ui::About) {
+    ui->setupUi(this);
+}
+About::~About() {
+    delete ui;
+}
+
+}// namespace evl
