546d3ed294a00abf35ba8329bd47f3bf20ef66d8
Silmaen <genteur.slayer@laposte.net>
2021-11-19 08:18:33 +0100

    packaging procedure

--- sources/main.cpp
+++ sources/main.cpp
@@ -8,15 +8,26 @@
 #include "gui/MainWindow.h"
 #include "gui/baseDefinitions.h"
 #include <QApplication>
+#include <QCommandLineOption>
+#include <QCommandLineParser>
 
 using namespace evl::gui;
 using namespace std::filesystem;
 
 int main(int argc, char* argv[]) {
     baseExecPath= absolute(path(argv[0])).parent_path();
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("Damien Lachouette");
+    QCoreApplication::setApplicationName("Evenemlent Loto");
+    QCoreApplication::setApplicationVersion("1.0");
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QCoreApplication::applicationName());
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.process(app);
+
     MainWindow window;
     window.syncSettings();
     window.show();
-    return QApplication::exec();
+    return app.exec();
 }
