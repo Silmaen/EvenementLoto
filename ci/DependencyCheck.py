0f921235fab8093ae6c601c1b67c0dfb24fb8137
Silmaen <genteur.slayer@laposte.net>
2023-11-06 08:23:11 +0100

    Updating dependencies (#14)

---
+++ ci/DependencyCheck.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+from pathlib import Path
+from sys import stderr
+
+dependencies = [
+    {"name": "magic_enum", "version": "*", "header": True},
+    {"name": "fmt", "version": "10.0.0", "header": False},
+    {"name": "spdlog", "version": "1.11.0", "header": False},
+    {"name": "googletest", "version": "*", "header": False},
+]
+
+
+class HostInfo:
+    def __init__(self):
+        self.os = "unknown"
+        self.arch = "unknown"
+        self.release = ""
+        self.libc = ""
+        self.__analyse()
+
+    def is_valid(self) -> bool:
+        if self.os in [None, "", "unknown"]:
+            return False
+        if self.arch in [None, "", "unknown"]:
+            return False
+        return True
+
+    def __analyse(self):
+        import platform
+        if Path("/etc/os-release").exists():
+            rel = {}
+            with open(Path("/etc/os-release"), "r") as fp:
+                lines = fp.readlines()
+                for line in lines:
+                    line = line.replace('"', '')
+                    it = line.split("=", 1)
+                    rel[it[0]] = it[1]
+            self.release = f"{rel['NAME']} {rel['VERSION']}"
+            self.libc = " ".join(platform.libc_ver())
+        self.os = platform.system()
+        self.arch = platform.machine()
+        if self.arch == "AMD64":
+            self.arch = "x86_64"
+
+
+hi = HostInfo()
+
+
+def has_depmanager():
+    import subprocess
+    try:
+        ret = subprocess.run("depmanager info version", shell=True).returncode
+        if ret != 0:
+            print(f"Error running depmanager ({ret}).", file=stderr)
+            exit(1)
+    except Exception as err:
+        print(f"Error Exception while running depmanager, {err}.", file=stderr)
+        exit(1)
+    return True
+
+
+def check_dependencies(target: dict):
+    from depmanager.api.package import PackageManager
+    if "os" not in target:
+        print("Error: no target os.", file=stderr)
+        exit(1)
+    if "arch" not in target:
+        print("Error: no target arch.", file=stderr)
+        exit(1)
+    if "kind" not in target:
+        print("Error: no target kind.", file=stderr)
+        exit(1)
+
+    # loc = LocalManager()
+    # pm = PackageManager(system=loc, verbosity=4)
+    pm = PackageManager(verbosity=4)
+
+    class Args:
+        default = True
+        name = ""
+
+    def_name = pm.remote_name(Args())
+    if target["kind"] == "both":
+        kinds = ["static", "shared"]
+    else:
+        kinds = [target["kind"]]
+
+    for kind in kinds:
+        for dep in dependencies:
+            query = {
+                "name": dep["name"],
+                "version": dep["version"],
+                "os": target["os"],
+                "arch": target["arch"],
+                "kind": "header"
+            }
+            if not dep["header"]:
+                query["kind"] = kind
+            # search local
+            result = pm.query(query, "")
+            if len(result) > 0:  # already in local
+                print(f"Dependency '{dep['name']} for {query['kind']}' found locally.")
+                continue
+            print(f"Dependency '{dep['name']} for {query['kind']}' not found locally.")
+            if def_name in ["", None]:
+                print(f"Error: No remote defined.", file=stderr)
+                exit(1)
+            result = pm.query(query, def_name)
+            if len(result) == 0:  # Not in remote
+                print(f"Error: Unable to find '{dep['name']} for {query['kind']}' on default remote.", file=stderr)
+                exit(1)
+            pm.add_from_remote(result[0], def_name)
+            # loc.clean_tmp()
+            # pm = PackageManager(verbosity=4)
+            result = pm.query(query, "")
+            if len(result) > 0:  # already in local
+                print(f"Dependency '{dep['name']} for {query['kind']}' successfully added to local.")
+            else:
+                print(f"Error: Unable to get '{dep['name']} for {query['kind']}' from default remote.", file=stderr)
+                exit(1)
+
+
+def check_remote(remote: dict):
+    if "url" not in remote:
+        return
+    if remote["url"] in ["", None]:
+        return
+    from depmanager.api.remotes import RemotesManager
+    remotes = RemotesManager(verbosity=4)
+    rem = remotes.get_safe_remote(name=remote["name"])
+    if rem is None:
+        url = remote["url"]
+        kind = "ftp"
+        if "://" in url:
+            kind, url = url.split("://", 1)
+            url = url.replace("/", "")
+        remotes.add_remote(name=remote["name"],
+                           url=url,
+                           kind=kind,
+                           default=True,
+                           login=remote["login"],
+                           passwd=remote["passwd"])
+
+
+def main():
+    from argparse import ArgumentParser as ap
+    arg_p = ap()
+    arg_p.add_argument("--kind", "-k",
+                       type=str,
+                       default="static",
+                       choices=["both", "static", "shared"],
+                       help="The targeted libraries kind.")
+    arg_p.add_argument("--os", "-o",
+                       type=str,
+                       default="",
+                       help="The targeted os.")
+    arg_p.add_argument("--arch", "-a",
+                       type=str,
+                       default="",
+                       help="The targeted architecture")
+    arg_p.add_argument("--remote-url", "-r",
+                       type=str,
+                       default="Remote url to use")
+    arg_p.add_argument("--remote-login", "-l",
+                       type=str,
+                       default="Remote login to use")
+    arg_p.add_argument("--remote-passwd", "-p",
+                       type=str,
+                       default="Remote password to use")
+    args = arg_p.parse_args()
+    has_depmanager()
+    remote = {
+        "name": "remote",
+        "url": args.remote_url,
+        "login": args.remote_login,
+        "passwd": args.remote_passwd
+    }
+    check_remote(remote)
+    target = {
+        "kind": args.kind,
+        "os": hi.os,
+        "arch": hi.arch
+    }
+    if args.os not in ["", None]:
+        target["os"] = args.os
+    if args.arch not in ["", None]:
+        target["arch"] = args.arch
+    check_dependencies(target)
+
+
+if __name__ == "__main__":
+    main()
---
+++ source/external/json.h
@@ -0,0 +1,21 @@
+/**
+ * @file json.h
+ * @author Silmaen
+ * @date 05/11/2023
+ * Copyright (c) 2023 All rights reserved.
+ * All modification must get authorization from the author.
+ */
+
+#pragma once
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcovered-switch-default"
+#pragma clang diagnostic ignored "-Wdeprecated-literal-operator"
+#pragma clang diagnostic ignored "-Wweak-vtables"
+#pragma clang diagnostic ignored "-Wnewline-eof"
+#endif
+#include "nlohmann/json.hpp"
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
